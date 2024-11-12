cd664465d4a6b36ab47ff1f7a243a663b16716a0
M Taha <tahasial321@gmail.com>
2024-11-12 22:57:43 +0500

    Add new files

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 const corsOptions = {
-  origin: `FRONTEND_URL`,
+  origin: "FRONTEND_URL",
   credentials: true,
 };
 
